7366cc1c5a675279dce2a4cb2a26612c4fbbae91
Vedant <vedantrahane01@gmail.com>
2019-03-20 12:44:00 -0400

    Check no is Palindrome or not- o(n)

---
+++ MyEPI/src/isPalindrome.java
@@ -0,0 +1,30 @@
+
+public class isPalindrome {
+
+  private static boolean isNumberPalindrome(int x) {
+    if (x <= 0) {
+      return x == 0;
+    }
+
+    final int numDigits = (int) (Math.floor(Math.log10(x))) + 1;
+    int msd = (int) Math.pow(10, numDigits - 1);
+
+    for (int i = 0; i < (numDigits / 2); ++i) {
+      if (x / msd != x % 10) {
+        return false;
+      }
+
+      x %= msd;   //Extracting MSB
+      x /= 10;    // Extracting LSB
+      msd /= 100;
+    }
+    return true;
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isNumberPalindrome(100));  // return false
+  }
+
+}
+
